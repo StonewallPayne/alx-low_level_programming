f4356aaa92c0f27846f26e4313a5511f78029d81
StonewallPayne <paynestonewall@gmail.com>
2022-06-09 16:51:10 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#inclue "main.h"
+
+/**
+ * mul -> multipying 2 numbers
+ * @a: integer param1
+ * @b: integer peram2
+ * Return: success and intiger
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
