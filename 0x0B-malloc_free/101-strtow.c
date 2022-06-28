dc31e74d4be0e22aa995d40e66902c3cb580f61d
StonewallPayne <paynestonewall@gmail.com>
2022-06-28 01:45:46 -0700

    hot fix

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -25,6 +25,31 @@ int word_len(char *str)
 }
 
 /**
+ * count_words - Counts the number of words contained within a string.
+ * @str: The string to be searched.
+ *
+ * Return: The number of words contained within str.
+ */
+int count_words(char *str)
+{
+	int index = 0, words = 0, len = 0;
+
+	for (index = 0; *(str + index); index++)
+		len++;
+
+	for (index = 0; index < len; index++)
+	{
+		if (*(str + index) != ' ')
+		{
+			words++;
+			index += word_len(str + index);
+		}
+	}
+
+	return (words);
+}
+
+/**
  * strtow - Splits a string into words.
  * @str: The string to be split.
  *
